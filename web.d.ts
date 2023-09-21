f435fa6399db8c315ac2dfe5e51ef28f89b99831
nin-jin <nin-jin@users.noreply.github.com>
2023-09-21 11:03:03 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- web.d.ts
+++ web.d.ts
@@ -5269,6 +5269,9 @@ declare namespace $.$$ {
         bookmarks(next?: readonly $hyoo_page_side[]): $hyoo_page_side[];
         pages_node(): $hyoo_crowd_list;
         pages(next?: readonly $hyoo_page_side[]): $hyoo_page_side[];
+        following(): $hyoo_page_side;
+        following_in(): $hyoo_page_side | null;
+        following_out(): $hyoo_page_side | null;
         bookmarked(id: $mol_int62_string, next?: boolean): boolean;
         authors(): `${string}_${string}`[];
         aura(next?: string): string;
@@ -5662,6 +5665,7 @@ declare namespace $ {
         side_release(): string;
         changed_moment(): $mol_time_moment;
         authors(): `${string}_${string}`[];
+        following(): $$.$hyoo_page_side;
         side(): $$.$hyoo_page_side;
         plugins(): readonly any[];
         auto(): readonly any[];
@@ -5700,6 +5704,7 @@ declare namespace $ {
         Author_link(id: any): $$.$hyoo_meta_link;
         author_list(): readonly any[];
         Author_list(): $mol_view;
+        Following(): $$.$hyoo_meta_link;
         Signature(): $mol_view;
     }
 }
@@ -5733,6 +5738,7 @@ declare namespace $.$$ {
         search_start(event?: KeyboardEvent): void;
         search_stop(event?: KeyboardEvent): void;
         details(): string;
+        Following(): $hyoo_meta_link;
         author_list(): $hyoo_meta_link[];
         slides_content(): string;
         slides_send(): void;
@@ -6277,6 +6283,66 @@ declare namespace $.$$ {
 }
 
 declare namespace $ {
+    class $mol_icon_dots_vertical extends $mol_icon {
+        path(): string;
+    }
+}
+
+declare namespace $ {
+    class $mol_select extends $mol_pick {
+        dictionary(next?: any): Record<string, any>;
+        options(): readonly string[];
+        value(next?: any): string;
+        option_label_default(): string;
+        Option_row(id: any): $mol_button_minor;
+        No_options(): $mol_view;
+        plugins(): readonly any[];
+        hint(): string;
+        bubble_content(): readonly any[];
+        Filter(): $$.$mol_string;
+        Trigger_icon(): $mol_icon_dots_vertical;
+        event_select(id: any, event?: any): any;
+        option_label(id: any): string;
+        filter_pattern(next?: any): string;
+        Option_label(id: any): $$.$mol_dimmer;
+        option_content(id: any): readonly any[];
+        no_options_message(): string;
+        nav_components(): readonly $mol_view[];
+        option_focused(component?: any): any;
+        nav_cycle(next?: any): boolean;
+        Nav(): $$.$mol_nav;
+        menu_content(): readonly $mol_view[];
+        Menu(): $$.$mol_list;
+        Bubble_pane(): $$.$mol_scroll;
+        submit(event?: any): any;
+        enabled(): boolean;
+    }
+}
+
+declare namespace $ {
+    function $mol_match_text<Variant>(query: string, values: (variant: Variant) => string[]): (variant: Variant) => boolean;
+}
+
+declare namespace $.$$ {
+    class $mol_select extends $.$mol_select {
+        filter_pattern(next?: string): string;
+        open(): void;
+        options(): readonly string[];
+        options_filtered(): readonly string[];
+        option_label(id: string): any;
+        option_rows(): $mol_button_minor[];
+        option_focused(component?: $mol_view): $mol_view | $mol_string | $mol_button_minor | null;
+        event_select(id: string, event?: MouseEvent): void;
+        nav_components(): ($mol_string | $mol_button_minor)[];
+        trigger_content(): readonly $mol_view_content[];
+        menu_content(): ($mol_view | $mol_button_minor)[];
+    }
+}
+
+declare namespace $ {
+}
+
+declare namespace $ {
     class $mol_icon_sync extends $mol_icon {
         path(): string;
     }
@@ -6289,27 +6355,36 @@ declare namespace $ {
 }
 
 declare namespace $ {
-    class $hyoo_sync_online extends $mol_link {
+    class $hyoo_sync_online extends $mol_select {
         minimal_width(): number;
         minimal_height(): number;
+        dictionary(): string[];
+        master_cursor(next?: any): number;
         yard(): $hyoo_sync_yard<unknown>;
-        uri(): string;
-        sub(): readonly any[];
-        attr(): Record<string, any>;
+        Filter(): any;
+        option_content(id: any): readonly any[];
+        trigger_content(): readonly any[];
+        master_id(id: any): string;
+        Option_logo(id: any): $$.$mol_avatar;
         master_link(): string;
         Well(): $$.$mol_avatar;
         Fail(): $mol_icon_sync_off;
+        link_content(): readonly any[];
         hint(): string;
         message(): string;
+        Link(): $$.$mol_link;
     }
 }
 
 declare namespace $.$$ {
     class $hyoo_sync_online extends $.$hyoo_sync_online {
         message(): string;
-        sub(): $mol_icon_sync_off[];
+        link_content(): $mol_icon_sync_off[];
         hint(): string;
         master_link(): string;
+        master_id(index: number): string;
+        option_label(index: number): string;
+        value(next?: string): string;
     }
 }
 
